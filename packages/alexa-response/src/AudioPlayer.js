7d07d968e61d7ce9659ef1586c60755b093757c2
Cameron Hunter <chunter@netflix.com>
2016-10-01 13:34:43 -0700

    Add support for AudioPlayer directive

---
+++ packages/alexa-response/src/AudioPlayer.js
@@ -0,0 +1,36 @@
+const AudioPlayer = {
+    Play: 'AudioPlayer.Play',
+    Stop: 'AudioPlayer.Stop',
+    ClearQueue: 'AudioPlayer.ClearQueue'
+};
+
+export default {
+    play(stream) {
+        return {
+            type: AudioPlayer.Play,
+            playBehavior: 'REPLACE_ALL',
+            audioItem: { stream }
+        };
+    },
+
+    enqueue(stream, replaceQueue = false) {
+        return {
+            type: AudioPlayer.Play,
+            playBehavior: replaceQueue ? 'REPLACE_ENQUEUED' : 'ENQUEUE',
+            audioItem: { stream }
+        };
+    },
+
+    stop() {
+        return {
+            type: AudioPlayer.Stop
+        };
+    },
+
+    clearQueue(stopCurrent = false) {
+        return {
+            type: AudioPlayer.ClearQueue,
+            clearBehavior: stopCurrent ? 'CLEAR_ALL' : 'CLEAR_ENQUEUED'
+        };
+    }
+};
