11ac591ddc3d95617c62c74976a27a74e0545927
Cameron Hunter <chunter@netflix.com>
2016-10-13 12:02:39 -0700

    Fix linting errors

--- packages/alexa-constants/src/index.js
+++ packages/alexa-constants/src/index.js
@@ -1,51 +1,51 @@
 export const Request = {
-    Intent: 'IntentRequest',
-    Launch: 'LaunchRequest',
-    SessionEnded: 'SessionEndedRequest'
+  Intent: 'IntentRequest',
+  Launch: 'LaunchRequest',
+  SessionEnded: 'SessionEndedRequest'
 };
 
 export const Intent = {
-    Cancel: 'AMAZON.CancelIntent',
-    Help: 'AMAZON.HelpIntent',
-    Stop: 'AMAZON.StopIntent',
-    LoopOff: 'AMAZON.LoopOffIntent',
-    LoopOn: 'AMAZON.LoopOnIntent',
-    Next: 'AMAZON.NextIntent',
-    No: 'AMAZON.NoIntent',
-    Pause: 'AMAZON.PauseIntent',
-    Previous: 'AMAZON.PreviousIntent',
-    Repeat: 'AMAZON.RepeatIntent',
-    Resume: 'AMAZON.ResumeIntent',
-    ShuffleOff: 'AMAZON.ShuffleOffIntent',
-    ShuffleOn: 'AMAZON.ShuffleOnIntent',
-    StartOver: 'AMAZON.StartOverIntent',
-    Yes: 'AMAZON.YesIntent'
+  Cancel: 'AMAZON.CancelIntent',
+  Help: 'AMAZON.HelpIntent',
+  Stop: 'AMAZON.StopIntent',
+  LoopOff: 'AMAZON.LoopOffIntent',
+  LoopOn: 'AMAZON.LoopOnIntent',
+  Next: 'AMAZON.NextIntent',
+  No: 'AMAZON.NoIntent',
+  Pause: 'AMAZON.PauseIntent',
+  Previous: 'AMAZON.PreviousIntent',
+  Repeat: 'AMAZON.RepeatIntent',
+  Resume: 'AMAZON.ResumeIntent',
+  ShuffleOff: 'AMAZON.ShuffleOffIntent',
+  ShuffleOn: 'AMAZON.ShuffleOnIntent',
+  StartOver: 'AMAZON.StartOverIntent',
+  Yes: 'AMAZON.YesIntent'
 };
 
 export const ConnectedHome = {
-    Control: 'Alexa.ConnectedHome.Control',
-    Discovery: 'Alexa.ConnectedHome.Discovery',
+  Control: 'Alexa.ConnectedHome.Control',
+  Discovery: 'Alexa.ConnectedHome.Discovery',
 
-    Request: {
-        DecrementPercentage: 'DecrementPercentageRequest',
-        DecrementTargetTemperature: 'DecrementTargetTemperatureRequest',
-        DiscoverAppliances: 'DiscoverAppliancesRequest',
-        IncrementPercentage: 'IncrementPercentageRequest',
-        IncrementTargetTemperature: 'IncrementTargetTemperatureRequest',
-        SetPercentage: 'SetPercentageRequest',
-        SetTargetTemperature: 'SetTargetTemperatureRequest',
-        TurnOff: 'TurnOffRequest',
-        TurnOn: 'TurnOnRequest'
-    },
+  Request: {
+    DecrementPercentage: 'DecrementPercentageRequest',
+    DecrementTargetTemperature: 'DecrementTargetTemperatureRequest',
+    DiscoverAppliances: 'DiscoverAppliancesRequest',
+    IncrementPercentage: 'IncrementPercentageRequest',
+    IncrementTargetTemperature: 'IncrementTargetTemperatureRequest',
+    SetPercentage: 'SetPercentageRequest',
+    SetTargetTemperature: 'SetTargetTemperatureRequest',
+    TurnOff: 'TurnOffRequest',
+    TurnOn: 'TurnOnRequest'
+  },
 
-    Confirmation: {
-        DecrementPercentage: 'DecrementPercentageConfirmation',
-        DecrementTargetTemperature: 'DecrementTargetTemperatureConfirmation',
-        IncrementPercentage: 'IncrementPercentageConfirmation',
-        IncrementTargetTemperature: 'IncrementTargetTemperatureConfirmation',
-        SetPercentage: 'SetPercentageConfirmation',
-        SetTargetTemperature: 'SetTargetTemperatureConfirmation',
-        TurnOff: 'TurnOffConfirmation',
-        TurnOn: 'TurnOnConfirmation'
-    }
+  Confirmation: {
+    DecrementPercentage: 'DecrementPercentageConfirmation',
+    DecrementTargetTemperature: 'DecrementTargetTemperatureConfirmation',
+    IncrementPercentage: 'IncrementPercentageConfirmation',
+    IncrementTargetTemperature: 'IncrementTargetTemperatureConfirmation',
+    SetPercentage: 'SetPercentageConfirmation',
+    SetTargetTemperature: 'SetTargetTemperatureConfirmation',
+    TurnOff: 'TurnOffConfirmation',
+    TurnOn: 'TurnOnConfirmation'
+  }
 };
