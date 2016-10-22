e1fa17bb9343c5a2533ea3d2436368be6f365757
Cameron Hunter <chunter@netflix.com>
2016-10-22 14:33:07 -0700

    Additional constants

--- packages/alexa-constants/src/ConnectedHome.js
+++ packages/alexa-constants/src/ConnectedHome.js
@@ -1,17 +1,24 @@
 export default {
   Control: 'Alexa.ConnectedHome.Control',
   Discovery: 'Alexa.ConnectedHome.Discovery',
+  System: 'Alexa.ConnectedHome.System',
 
   Request: {
     DecrementPercentage: 'DecrementPercentageRequest',
     DecrementTargetTemperature: 'DecrementTargetTemperatureRequest',
     DiscoverAppliances: 'DiscoverAppliancesRequest',
+    HealthCheck: 'HealthCheckRequest',
     IncrementPercentage: 'IncrementPercentageRequest',
     IncrementTargetTemperature: 'IncrementTargetTemperatureRequest',
     SetPercentage: 'SetPercentageRequest',
     SetTargetTemperature: 'SetTargetTemperatureRequest',
     TurnOff: 'TurnOffRequest',
     TurnOn: 'TurnOnRequest'
+  },
+
+  Response: {
+    DiscoverAppliances: 'DiscoverAppliancesResponse',
+    HealthCheck: 'HealthCheckResponse'
   },
 
   Confirmation: {
