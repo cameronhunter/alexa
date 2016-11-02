0dcc6a380e6364a12277e73c17e56fd9f83ef8b7
Cameron Hunter <chunter@netflix.com>
2016-11-02 13:39:50 -0700

    Add support for custom SSML JSX tags

--- packages/alexa-ssml-jsx/src/ssml.js
+++ packages/alexa-ssml-jsx/src/ssml.js
@@ -2,7 +2,7 @@ import schema from './schema/index';
 import validate from './prop-types/validate';
 
 export default function ssml(tagName, props, ...children) {
-  const tag = throwIfUndefined(schema[tagName], `Unsupported tag: "${tagName}"`);
+  const tag = throwIfUndefined(getTagDefinition(tagName), `Unsupported tag: "${tagName}"`);
 
   const hasProps = props || tag.defaultProps || children.length;
   const mergedProps = hasProps && { ...tag.defaultProps, ...props, ...(children.length && { children }) };
@@ -16,5 +16,16 @@ function throwIfUndefined(item, error) {
     throw new Error(error);
   } else {
     return item;
+  }
+}
+
+function getTagDefinition(tag) {
+  switch (typeof tag) {
+  case 'string':
+    return schema[tag];
+  case 'function':
+    return { ...tag, type: tag };
+  default:
+    return undefined;
   }
 }
--- packages/alexa-ssml-jsx/test/ssml-test.js
+++ packages/alexa-ssml-jsx/test/ssml-test.js
@@ -21,6 +21,18 @@ test('creates an object containing type and props keys', t => {
   });
 });
 
+test('handles custom tags', t => {
+  const Format = (props) => Object.entries(props).reduce((state, [k, v]) => state.replace('{' + k + '}', v), props.template);
+  const speech = (<Format template='Hello {name}' name='world' />);
+  t.deepEqual(speech, {
+    type: Format,
+    props: {
+      template: 'Hello {name}',
+      name: 'world'
+    }
+  });
+});
+
 test('creates an object containing type and children keys', t => {
   const speech = (
         <speak>
