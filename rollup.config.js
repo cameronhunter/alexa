39315d7776af78f4ba5d5af1e262a762cf7025b3
Cameron Hunter <chunter@netflix.com>
2017-04-18 11:45:02 -0700

    Update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,24 @@
 import babel from 'rollup-plugin-babel';
-import uglify from 'rollup-plugin-uglify';
-import inject from 'rollup-plugin-inject';
 
 const isProduction = process.env.NODE_ENV === 'production';
-const nonNull = array => array.filter(Boolean);
 
 export default {
   entry: 'src/index.js',
   dest: isProduction ? './build/index.min.js' : './build/index.js',
   format: 'cjs',
-  plugins: nonNull([
+  exports: 'named',
+  external: ['uuid', 'alexa-constants', 'alexa-ssml-jsx'],
+  plugins: [
     babel({
       babelrc: false,
-      presets: ['es2015-rollup', 'stage-1'],
-      plugins: ['transform-decorators-legacy', 'transform-class-properties']
-    }),
-    inject({
-      exclude: 'node_modules/**',
-      Promise: 'bluebird',
-      'Object.values': 'object-values'
-    }),
-    isProduction ? uglify() : null
-  ])
+      presets: [['env', { targets: { node: 6.10 }, modules: false }]],
+      plugins: [
+        'transform-decorators-legacy',
+        'transform-class-properties',
+        'transform-object-rest-spread',
+        'transform-export-extensions',
+        ['transform-react-jsx', { pragma: 'ssml' }]
+      ]
+    })
+  ]
 };
